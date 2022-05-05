2a40da5572df565e3196ed137cdfa6614031399b
epsiba <epsibaranis130897@gmail.com>
2022-05-05 22:16:45 +0530

    first commit

---
+++ pgm11.py
@@ -0,0 +1,5 @@
+#area of triangle
+b=int(input('b=?'))
+h=int(input('h=?'))
+a=1/2*b*h
+print(a)
---
+++ pgm15.py
@@ -0,0 +1,8 @@
+#reverse a three digit no
+a=int(input('a=?'))
+c=a%10
+d=a//10
+e=d%10
+f=d//10
+h=c*100+e*10+f
+print(h)
